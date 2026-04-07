91fc7527fcc2c9d46ec9100eb35a5cfb3706ad76
George Mastrogiannis <giorgos_M000@hotmail.com>
2026-04-07 14:21:15 +0300

    Fix homepage URL: changed base path to /ece_blog/ and updated canonical/meta tags

---
+++ dist/assets/external-link-Brtfi2vi.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-SeMyQK3A.js";/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],h=a("arrow-right",t);/**
+ * @license lucide-react v0.546.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M15 3h6v6",key:"1q9fwt"}],["path",{d:"M10 14 21 3",key:"gplh6r"}],["path",{d:"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",key:"a6xqqp"}]],r=a("external-link",e);export{h as A,r as E};
